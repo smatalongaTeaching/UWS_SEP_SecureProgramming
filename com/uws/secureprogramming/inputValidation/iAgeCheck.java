6587c83aa29f776e14a3cfb2a2494effa11e566d
Santiago Matalonga <santiago.matalonga@uws.ac.uk>
2025-06-12 13:05:09 +0100

    Excercise 1 upload

---
+++ com/uws/secureprogramming/inputValidation/iAgeCheck.java
@@ -0,0 +1,7 @@
+package com.uws.secureprogramming.inputValidation;
+
+public interface iAgeCheck {
+    final String Adult = "Adult";
+    final String Minor = "Minor";
+    String checkAge(String age);
+}
